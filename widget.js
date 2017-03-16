bd78577d7dc5a78fd23281999113f32cce9389b3
Dawn Monroe <deetriplett@gmail.com>
2017-03-16 12:43:46 -0400

    Update widget.js

--- widget.js
+++ widget.js
@@ -1,64 +1,111 @@
-//API Request from SWAPI//
+//RETRIEVE API JSON and DISPLAY 
 
-const url = "http://swapi.co/api/people/?page=1";
+const  xhr = new XMLHttpRequest();
 
-let xhr = new XMLHttpRequest();
 xhr.onreadystatechange = function () {
   if (xhr.readyState === 4 && xhr.status === 200) {
     let people = JSON.parse(xhr.responseText).results;
-    console.log(people);
-    //Display Data
+    //console.log(people);
+  
+    //DISPLAY DATA
     let statusHTML = '<ul>';
-    //Loop through JSON Data for how many results there are (10)
+    //Loop through JSON Data for how many results there are 
     for (var i=0; i<people.length; i++) {
-      console.log(i, people[i])
+      console.log(i, people[i]);
       // Only create *one* <li> tag per person
       statusHTML += '<li>';
-      statusHTML += people[i].name + ', '
-      statusHTML += people[i].birth_year + ', ';
-      statusHTML += people[i].gender + ', ';
-      statusHTML += people[i].homeworld + ', ';
-      statusHTML += people[i].species + ', ';
-      statusHTML += people[i].url;
+      statusHTML += '<h3>' + people[i].name + '</h3><br>';
+      statusHTML += '<h4>Birth Year : </h4>' + people[i].birth_year + '   ';
+      statusHTML += '<h4>Gender : </h4>' + people[i].gender + '<br>';
+      //HREF
+      statusHTML += '<a href =""> <p>Check out Homeworld : </p>';
+      statusHTML += people[i].homeworld + '</a><br>';
+      statusHTML += '<a href =""> <p>Check out Species : </p>';
+      statusHTML += people[i].species + '</a><br>';
+      statusHTML += '<a href =""> <p>Character URL : </p>';
+      statusHTML += people[i].url + '</a>';
       statusHTML += '</li>'
     }
-      statusHTML += '</ul>';
+      statusHTML += '</ul><br>';
+     // statusHTML += '<button id="next"> Next </button>';
       //INSERT into HTML
-      console.log(statusHTML);
+      //console.log(statusHTML);
       document.getElementById('people-list').innerHTML = statusHTML;
   };
 }//End ONREADY
 
-xhr.open('GET', url);
+xhr.open('GET', 'http://swapi.co/api/people/?page=1');
 xhr.send();
-///////////////////////////////////////////////////////
 
-let ajax2 = new XMLHttpRequest();
-ajax2.onreadystatechange = function () {
-  if (ajax2.readyState === 4 && ajax2.status === 200) {
-    let morePeople = JSON.parse(ajax2.responseText).results;
-    console.log(morePeople);
-    //Display Data
-    let statusHTML = '<ul>';
-    //Loop through JSON Data for how many results there are (10)
-    for (var i=0; i<morePeople.length; i++) {
-      console.log(i, morePeople[i])
-      // Only create *one* <li> tag per person
-      statusHTML += '<li>';
-      statusHTML += morePeople[i].name + ', '
-      statusHTML += morePeople[i].birth_year + ', ';
-      statusHTML += morePeople[i].gender + ', ';
-      statusHTML += morePeople[i].homeworld + ', ';
-      statusHTML += morePeople[i].species + ', ';
-      statusHTML += morePeople[i].url;
-      statusHTML += '</li>'
-    }
-      statusHTML += '</ul>';
-      //INSERT into HTML
-      console.log(statusHTML);
-      document.getElementById('morePeople-list').innerHTML = statusHTML;
-  };
-}//End ONREADY
 
-ajax2.open('GET', 'http://swapi.co/api/people/?page=2');
-ajax2.send();
+///////////////////////////////////////////////////////////
+//JQuery
+ 
+//REVEAL button
+$('#reveal').click(function () {
+  $('#people-list ul li').show();
+  $('#reveal').hide();  
+});
+
+
+
+
+//Retrieve(GET) data from http://swapi.co/api/people
+  //1. Store response to a global 'PEOPLE' variable so it's accessible anywhere, not just inside that callback function.
+  //2. Create a function that will take an index as a parameter
+//Parse JSON
+   // Render the HTML for just one person object
+   //Inject into DOM
+   //Button cLick through data
+
+
+// let xhr = $.get( "http://swapi.co/api/people", function() {
+//   console.log( "success" );
+// })
+//   .done(function() {
+//     console.log( "second success" );
+//   })
+//   .fail(function() {
+//     console.log( "error" );
+//   })
+//   .always(function() {
+//     console.log( "finished" );
+//   });
+
+//Parse JSON Data into an array variable
+// var arrayOfObjects = [{}]
+
+// $(jQuery.parseJSON(JSON.stringify(arrayOfObjects))).each(function() {
+//     var ID = this.id;
+//     var TITLE = this.Title;
+// });
+
+//Loop through array
+
+// for (var i = 0; i < arrayOfObjects.length; i++) {
+//     var object = arrayOfObjects[i];
+//     for (var property in object) {
+            //html-ify it
+//     }}
+
+// data = []
+//     //key: property
+//     "name" : people[i].name,
+//     "year" : people[i].birth_year,
+//     "gender" : people[i].gender,
+//     "homeworld" : people[i].homeworld,
+//     "species" : people[i].species,
+//     "url" : people[i].url
+//   ];
+// console.log(data)
+
+// let Person = function(data) {
+//   for (key in data) {
+//     this[key] = data[key];
+//   }
+//   this.getKeys = function () {
+//     return Object.keys(this);
+//   }
+// }
+
+//PROMISES!
